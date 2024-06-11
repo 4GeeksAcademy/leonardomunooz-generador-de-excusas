591ecf26ea1443b7cf187b9526c6aa168d3f04de
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-06-11 00:43:39 +0000

    se ha realizado el ejercicio del excuse generator y se ha modificado el index.html para mostrar el mensaje

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusaGenerada = excuseGenerator(who, action, what, when);
+  let excusa = document.getElementById("excusa");
+  excusa.innerHTML = excusaGenerada;
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGenerator(whoRandom, actionRandom, whatRandom, whenRandom) {
+  let who = Math.floor(Math.random() * whoRandom.length);
+  let action = Math.floor(Math.random() * actionRandom.length);
+  let what = Math.floor(Math.random() * whatRandom.length);
+  let when = Math.floor(Math.random() * whenRandom.length);
+  return `${whoRandom[who]}  ${actionRandom[action]} ${whatRandom[what]} ${whenRandom[when]} `;
+}
